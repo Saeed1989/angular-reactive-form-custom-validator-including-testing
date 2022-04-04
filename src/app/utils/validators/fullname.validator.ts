2400f8594cb816d008562d6d158af4ffae29b807
saeed1 <saeed.sharman@gmail.com>
2022-04-04 21:41:46 +0900

    adds error message

--- src/app/utils/validators/fullname.validator.ts
+++ src/app/utils/validators/fullname.validator.ts
@@ -1,5 +1,5 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ValidateFullName(control: AbstractControl) {
-  return control.value.split(" ").length > 1 ?  null : { invalidName: true };
+  return control.value.split(" ").length === 2 ?  null : { invalidName: true };
 }
