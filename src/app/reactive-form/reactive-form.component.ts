2993d557c11ae2a454e6a570fe14d7fd04327be9
saeed1 <saeed.sharman@gmail.com>
2022-04-04 21:11:11 +0900

    applies reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
+  contactForm = new FormGroup({
+    fullName: new FormControl(),
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
   }
 
 }
