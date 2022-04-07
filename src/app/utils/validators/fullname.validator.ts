5d92ab70ea07a868af304189c15f767de5138705
saeed1 <saeed.sharman@gmail.com>
2022-04-07 20:19:19 +0100

    adds more validation

--- src/app/utils/validators/fullname.validator.ts
+++ src/app/utils/validators/fullname.validator.ts
@@ -1,16 +1,27 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ValidateFullName(control: AbstractControl) {
-  let lenth = control.value.split(' ').length;
+  let length = control.value.split(' ').length;
   let error = null;
-  if (lenth === 2) {
-    error = null;
-  } else if (lenth === 1 && control.value?.length > 0) {
+
+  if (length === 2) {
+    const values = control.value.split(' ');
+    if (values[0].length > 10) {
+      error = { errorMessage: 'Length of first name must be less than 10 character' };
+    } else if (values[0].length === 0) {
+      error = { errorMessage: 'Please provide first name' };
+    } else if (values[1].length > 10) {
+      error = { errorMessage: 'Length of second name must be less than 10 character' };
+    } else if (values[1].length === 0) {
+      error = { errorMessage: 'Please provide second name' };
+    }
+  } else if (length === 1 && control.value?.length > 0) {
     error = { errorMessage: 'Please provide second name' };
-  } else if (lenth > 2) {
+  } else if (length > 2) {
     error = { errorMessage: 'Only single space is allowed' };
   } else {
     error = { errorMessage: 'Please provide full name' };
   }
+
   return error;
 }
